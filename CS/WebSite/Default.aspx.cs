34c2a8c4c33afb788aa1f0acb0c0a51e5b2a913d
DevExpressExampleBot <fakeEmail>
2022-12-07 19:38:19 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -3,7 +3,7 @@
         XtraReport1.IsReportExporting = (Page.Request["__EVENTARGUMENT"] != null && (Page.Request["__EVENTARGUMENT"].IndexOf("saveToDisk") != -1 || Request["__EVENTARGUMENT"].IndexOf("saveToWindow") != -1));
     }
 
-    protected void ASPxCallbackPanel1_Callback(object source, DevExpress.Web.ASPxClasses.CallbackEventArgsBase e) {
+    protected void ASPxCallbackPanel1_Callback(object source, DevExpress.Web.CallbackEventArgsBase e) {
         XtraReport1.UpdateRecordsState(System.Convert.ToInt32(e.Parameter));
     }
 }
