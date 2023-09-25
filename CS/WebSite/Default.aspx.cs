4f886f9138ee383dedf55a702a14eb1bd8b254e5
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 23:38:58 +0400

    Change example status to out of maintenance

--- CS/WebSite/Default.aspx.cs
+++
@@ -1,9 +0,0 @@
-﻿public partial class _Default : System.Web.UI.Page {
-    protected void Page_Load(object sender, System.EventArgs e) {
-        XtraReport1.IsReportExporting = (Page.Request["__EVENTARGUMENT"] != null && (Page.Request["__EVENTARGUMENT"].IndexOf("saveToDisk") != -1 || Request["__EVENTARGUMENT"].IndexOf("saveToWindow") != -1));
-    }
-
-    protected void ASPxCallbackPanel1_Callback(object source, DevExpress.Web.CallbackEventArgsBase e) {
-        XtraReport1.UpdateRecordsState(System.Convert.ToInt32(e.Parameter));
-    }
-}
